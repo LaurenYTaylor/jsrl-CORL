19983457be8077c062bb72b2e0eee46890447f71
LaurenYTaylor <lauren.taylor@adelaide.edu.au>
2025-02-06 15:48:55 +1030

    fixed success metric

--- algorithms/finetune/iql.py
+++ algorithms/finetune/iql.py
@@ -228,6 +228,8 @@ def wandb_init(config: dict) -> None:
 def is_goal_reached(reward: float, info: Dict) -> bool:
     if "goal_achieved" in info:
         return info["goal_achieved"]
+    elif "success" in info:
+        return info["success"]
     return reward > 0  # Assuming that reaching target is a positive reward
 
 
