02e7426b334cf2e16633e9c8320b46c8123cfcf1
Lauren Taylor <a1899783@adelaide.edu.au>
2025-02-07 15:38:44 +1030

    fixed move_offline_agent not overwriting

--- move_offline_agent.py
+++ move_offline_agent.py
@@ -14,7 +14,7 @@ for folder in newest_to_oldest:
     folder_files = glob.glob(f"{folder}/*.pt")
     if args.env in folder and f"{folder}/checkpoint_999999.pt" in folder_files:
         print(f"Moving folder {folder}")
-        shutil.copytree(folder, folder[:-8]+"offline")
+        shutil.copytree(folder, folder[:-8]+"offline", dirs_exist_ok=True)
         break
     
 
